973dd707cde4e1df1f5394f6145ed62fa263b624
Adrián Brito <adrbrpa1988@gmail.com>
2017-06-21 18:52:08 +0200

    Updated touchstart event functionality

--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 /**
  * Generated class for the SliderComponent component.
@@ -13,6 +13,10 @@ import { Component } from '@angular/core';
 
 export class SliderComponent {
 
+    @ViewChild('cdhandle')
+    @ViewChild('cdimagecontainer')
+    cdhandle: ElementRef;
+    cdimagecontainer: ElementRef;
     flagDrag: boolean = false;
     flagResize: boolean = false;
 
@@ -24,6 +28,30 @@ export class SliderComponent {
         console.log("Touchstart");
         this.flagDrag = true;
         this.flagResize = true;
+
+
+        console.log(this.cdhandle);
+
+        let dragWidth = this.cdhandle.nativeElement.offsetWidth;
+        console.log("dragWidth: " + dragWidth);
+
+        console.log(event.touches[0].pageX);
+
+        let xPosition = this.cdhandle.nativeElement.offsetLeft + dragWidth - event.touches[0].pageX;
+        console.log("xPositiom: " + xPosition);
+
+        console.log(this.cdimagecontainer);
+
+
+
+        /*
+            xPosition = dragElement.offset().left + dragWidth - e.pageX,
+            containerOffset = container.offset().left,
+            containerWidth = container.outerWidth(),
+            minLeft = containerOffset + 10,
+            maxLeft = containerOffset + containerWidth - dragWidth - 10;
+            */
+
     }
 
     touchmove(event) {
