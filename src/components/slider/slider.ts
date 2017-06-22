be60b0f289e74d7150c668461abd90f8355f66d4
Adrián Brito <adrbrpa1988@gmail.com>
2017-06-22 11:13:47 +0200

    Updated touchmove functionality

--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -13,12 +13,18 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 
 export class SliderComponent {
 
-    @ViewChild('cdhandle')
-    @ViewChild('cdimagecontainer')
-    cdhandle: ElementRef;
-    cdimagecontainer: ElementRef;
+    @ViewChild('slider') slider: ElementRef;
+    @ViewChild('cdhandle') cdhandle: ElementRef;
     flagDrag: boolean = false;
     flagResize: boolean = false;
+    dragWidth: number;
+    xPosition: number;
+    containerOffset: number;
+    containerWidth: number;
+    minLeft: number;
+    maxLeft: number;
+    leftValue: number;
+    widthValue: string;
 
     constructor() {
         console.log('Hello SliderComponent Component');
@@ -32,34 +38,51 @@ export class SliderComponent {
 
         console.log(this.cdhandle);
 
-        let dragWidth = this.cdhandle.nativeElement.offsetWidth;
-        console.log("dragWidth: " + dragWidth);
+        this.dragWidth = this.cdhandle.nativeElement.offsetWidth;
+        console.log("dragWidth: " + this.dragWidth);
 
         console.log(event.touches[0].pageX);
 
-        let xPosition = this.cdhandle.nativeElement.offsetLeft + dragWidth - event.touches[0].pageX;
-        console.log("xPositiom: " + xPosition);
+        this.xPosition = this.cdhandle.nativeElement.offsetLeft + this.dragWidth - event.touches[0].pageX;
+        console.log("xPositiom: " + this.xPosition);
 
-        console.log(this.cdimagecontainer);
+        console.log(this.slider);
 
+        this.containerOffset = this.slider.nativeElement.offsetLeft;
+        console.log("containerOffset: " + this.containerOffset);
 
+        this.containerWidth = this.slider.nativeElement.offsetWidth;
+        console.log("containerWidth: " + this.containerWidth);
 
-        /*
-            xPosition = dragElement.offset().left + dragWidth - e.pageX,
-            containerOffset = container.offset().left,
-            containerWidth = container.outerWidth(),
-            minLeft = containerOffset + 10,
-            maxLeft = containerOffset + containerWidth - dragWidth - 10;
-            */
-
+        this.minLeft = this.containerOffset + 10;
+        console.log("minLeft: " + this.minLeft);
+        this.maxLeft = this.containerOffset + this.containerWidth - this.dragWidth - 10;
+        console.log("maxLeft: " + this.maxLeft);
     }
 
     touchmove(event) {
         console.log("Touchmove");
+
+        this.leftValue = event.touches[0].pageX + this.xPosition - this.dragWidth;
+        console.log("leftValue: " + this.leftValue);
+
+        // Constrain the draggable element to move inside his container.
+        if (this.leftValue < this.minLeft) {
+            this.leftValue = this.minLeft;
+        } else if ( this.leftValue > this.maxLeft) {
+            this.leftValue = this.maxLeft;
+        }
+
+        this.widthValue = (this.leftValue + this.dragWidth/2 - this.containerOffset) * 100/this.containerWidth + '%';
+        console.log("widthValue: " + this.widthValue);
+
+
     }
 
     touchend(event) {
         console.log("Touchend");
+        this.flagDrag = false;
+        this.flagResize = false;
     }
 
 
